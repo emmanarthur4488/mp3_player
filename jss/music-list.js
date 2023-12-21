95db756c5f949700d55c398e69c5d48e5772b909
emmanarthur488 <arthurgodswill56@gmail.com>
2023-12-21 16:21:35 +0100

    first commit

---
+++ jss/music-list.js
@@ -0,0 +1,120 @@
+let allMusic = [
+    {
+        name: "Best of Me",
+        artist: "Jordan",
+        img: "music-1",
+        src: "music-1",
+    },
+
+    {
+        name: "On a Regular",
+        artist: "Mercy Chinwo",
+        img: "music-2",
+        src: "music-2",
+    },
+
+    {
+        name: "Hold on to me",
+        artist: "Jordan",
+        img: "music-3",
+        src: "music-3",
+    },
+
+    {
+        name: "Hear my Cry",
+        artist: "Jordan",
+        img: "music-4",
+        src: "music-4",
+    },
+
+    {
+        name: "Standing my ground",
+        artist: "Jordan",
+        img: "music-5",
+        src: "music-5",
+    },
+
+    {
+        name: "Udeme me",
+        artist: "Mercy Chinwo",
+        img: "music-6",
+        src: "music-6",
+    },
+
+    {
+        name: "If you beleive",
+        artist: "Jordan",
+        img: "music-7",
+        src: "music-7",
+    },
+
+    {
+        name: "I feel the pull inside",
+        artist: "Jordan",
+        img: "music-8",
+        src: "music-8",
+    },
+
+    {
+        name: "Life is a big fight",
+        artist: "Tatiana Manaois",
+        img: "music-9",
+        src: "music-9",
+    },
+
+    {
+        name: "Not by might",
+        artist: "crabby",
+        img: "music-10",
+        src: "music-10",
+    },
+
+    {
+        name: "A prisoner",
+        artist: "Jord",
+        img: "music-11",
+        src: "music-11",
+    },
+
+    {
+        name: "A prisoner",
+        artist: "Hills top",
+        img: "music-12",
+        src: "music-12",
+    },
+
+    {
+        name: "yes he supplies",
+        artist: "Hills top",
+        img: "music-13",
+        src: "music-13",
+    },
+
+    {
+        name: "Arise possess your inheritance",
+        artist: "Clevered",
+        img: "music-14",
+        src: "music-14",
+    },
+
+    {
+        name: "I'm a pligrim",
+        artist: "crabby family",
+        img: "music-15",
+        src: "music-15",
+    },
+
+    {
+        name: "The only one",
+        artist: "Hills top",
+        img: "music-16",
+        src: "music-16",
+    },
+
+    {
+        name: "That sounds like home to me",
+        artist: "Hills top",
+        img: "music-17",
+        src: "music-17",
+    },
+]
